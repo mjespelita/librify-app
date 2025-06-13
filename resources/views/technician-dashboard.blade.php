6a49e66cb9efad626aea0a0ea413074852721a53
Mark Jason Penote Espelita <mjespelita.swashed@gmail.com>
2025-06-13 10:16:08 +0800

    v2.0.1

--- resources/views/technician-dashboard.blade.php
+++ resources/views/technician-dashboard.blade.php
@@ -2,61 +2,122 @@
 
 @section('content')
     <h1>Dashboard</h1>
-    <b>Hello, Technician - {{ Auth::user()->name }}</b>
-    <div class="row">
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">On Site Items</h5>
-                    <h1>
-                        <i class="fas fa-boxes"></i>
-                        {{ App\Models\Onsites::where('technicians_id', Auth::user()->id)->sum('quantity') }}
-                    </h1>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Damaged Items</h5>
-                    <h1>
-                        <i class="fas fa-house"></i>
-                        {{ App\Models\Damages::where('technicians_id', Auth::user()->id)->sum('quantity') }}
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </div>
+    <b>Hello, {{ Auth::user()->name }}</b>
 
-    <h4>Site Maps</h4>
-    <div class="row">
-        @forelse (App\Models\Sites::where('users_id', Auth::user()->id)->orderBy('id', 'desc')->get() as $item)
-            <div class="col-lg-4 col-md-4 col-sm-12">
+    @if (Auth::user()->role === 'technician')
+        <div class="row">
+            <div class="col-md-4">
                 <div class="card">
                     <div class="card-body">
-                        <b>{{ $item->name }}</b> <br>
-                        <div>
-                            <iframe
-                                width="100%"
-                                height="300"
-                                src="{{ $item->google_map_link }}"
-                                allowfullscreen>
-                            </iframe>
+                        <h5 class="card-title">On Site Items</h5>
+                        <h1>
+                            <i class="fas fa-boxes"></i>
+                            {{ App\Models\Onsites::where('technicians_id', Auth::user()->id)->sum('quantity') }}
+                        </h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Damaged Items</h5>
+                        <h1>
+                            <i class="fas fa-house"></i>
+                            {{ App\Models\Damages::where('technicians_id', Auth::user()->id)->sum('quantity') }}
+                        </h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Pending Tasks</h5>
+                        <h1>
+                            <i class="fas fa-boxes"></i>
+                            {{ App\Models\Taskassignments::where('users_id', Auth::user()->id)->whereHas('tasks', function ($query) {
+                                $query->where('status', 'pending');
+                            })->count() }}
+                        </h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Completed Tasks</h5>
+                        <h1>
+                            <i class="fas fa-boxes"></i>
+                            {{ App\Models\Taskassignments::where('users_id', Auth::user()->id)->whereHas('tasks', function ($query) {
+                                $query->where('status', 'completed');
+                            })->count() }}
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <h4>Site Maps</h4>
+        <div class="row">
+            @forelse (App\Models\Sites::where('users_id', Auth::user()->id)->orderBy('id', 'desc')->get() as $item)
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <b>{{ $item->name }}</b> <br>
+                            <div>
+                                <iframe
+                                    width="100%"
+                                    height="300"
+                                    src="{{ $item->google_map_link }}"
+                                    allowfullscreen>
+                                </iframe>
+                            </div>
+                            <a href="{{ url('show-sites/'.$item->id) }}">
+                                <button class="btn btn-outline-secondary"><i class="fas fa-eye"></i> View Details</button>
+                            </a>
                         </div>
-                        <a href="{{ url('show-sites/'.$item->id) }}">
-                            <button class="btn btn-outline-secondary"><i class="fas fa-eye"></i> View Details</button>
-                        </a>
                     </div>
                 </div>
-            </div>
-        @empty
-            <div class="col-lg-3 col-md-4 col-sm-12">
+            @empty
+                <div class="col-lg-3 col-md-4 col-sm-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4>No Sites....</h4>
+                        </div>
+                    </div>
+                </div>
+            @endforelse
+        </div>
+    @endif
+
+    @if (Auth::user()->role === 'employee')
+
+        <div class="row">
+            <div class="col-md-4">
                 <div class="card">
                     <div class="card-body">
-                        <h4>No Sites....</h4>
+                        <h5 class="card-title">Pending Tasks</h5>
+                        <h1>
+                            <i class="fas fa-boxes"></i>
+                            {{ App\Models\Taskassignments::where('users_id', Auth::user()->id)->whereHas('tasks', function ($query) {
+                                $query->where('status', 'pending');
+                            })->count() }}
+                        </h1>
                     </div>
                 </div>
             </div>
-        @endforelse
-    </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Completed Tasks</h5>
+                        <h1>
+                            <i class="fas fa-boxes"></i>
+                            {{ App\Models\Taskassignments::where('users_id', Auth::user()->id)->whereHas('tasks', function ($query) {
+                                $query->where('status', 'completed');
+                            })->count() }}
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
 @endsection
